9f316ed037b7b25b40575c35ca39252eee4d8ba3
Eureka <eureka@hana.ci>
2023-10-04 16:28:55 +0700

    custom_modules: Introduce some of custom modules
    Signed-off-by: Eureka <eureka@hana.ci>

---
+++ pyrobud/custom_modules/cgss.py
@@ -0,0 +1,70 @@
+# Copyright 2023, Dicky Herlambang "Nicklas373" <herlambangdicky5@gmail.com>
+# Copyright 2016-2023, HANA-CI Build Project
+# SPDX-License-Identifier: GPL-3.0-or-later
+# This module based on https://github.com/Nicklas373/CGSS_ACB_Downloader that only
+# have minimal function to track DB manifest version and report to user.
+
+import telethon as tg
+import requests
+import json
+from .. import command, module, util
+
+class CgssModule(module.Module):
+    name = "CGSS"
+    # disabled = True
+
+    db: util.db.AsyncDB
+
+    async def on_load(self) -> None:
+        self.db = self.bot.get_db("cgss")
+
+    async def on_message(self, event: tg.events.NewMessage.Event) -> None:
+        pass
+
+    @command.desc("Get information about latest manifest version from CGSS")
+    @command.alias("get_manifest")
+    async def cmd_cgss(self, ctx: command.Context) -> str:
+        version=None
+        verbose=True
+
+        await ctx.respond("Loading...")\
+
+        if not version:
+            if verbose:
+                try:
+                    await ctx.respond("Getting game version ...")
+                    url="https://starlight.kirara.ca/api/v1/info"
+                    r=requests.get(url)
+                    jsonData=json.loads(r.content)
+                    version=jsonData['truth_version']
+                except Exception as e:
+                    await ctx.respond("No DB service was available!...")
+                    version="NULL"
+                else:
+                    await ctx.respond("Getting game version from esterTion source...")
+                    url="https://raw.githubusercontent.com/esterTion/cgss_master_db_diff/master/!TruthVersion.txt"
+                    r=requests.get(url)
+                    version=r.text.rstrip()
+
+                await ctx.respond("Getting game version from local repository...")
+                url="https://raw.githubusercontent.com/Nicklas373/CGSS_ACB_Downloader/master/Static_version"
+                r=requests.get(url)
+                old_version=r.text.rstrip()
+
+                if (version > old_version):
+                    reason="Local DB older than Dynamic DB!"
+                elif (version == old_version):
+                    reason="Local DB on latest version!"
+                elif (version < old_version):
+                    reason="Local DB higher than Dynamic DB, ERROR"
+
+                result=(
+                        f"**CGSS DB Manifest Ver.**\n"
+                        f"\n"
+                        f"**Local DB Ver.** : `{old_version}`\n"
+                        f"**Online DB Ver.** : `{version}`\n"
+                        f"**Status** : `{reason}`\n"
+                        f"\n"
+                        )
+                await ctx.respond(result)
+
---
+++ pyrobud/custom_modules/vpsinfo.py
@@ -0,0 +1,69 @@
+import json
+import telethon as tg
+import psutil
+import subprocess
+from .. import command, module, util
+
+class vpsinfo(module.Module):
+    name = "VPS Info"
+
+    db: util.db.AsyncDB
+
+    async def on_load(self) -> None:
+        self.db = self.bot.get_db("vpsinfo")
+
+    async def on_message(self, event: tg.events.NewMessage.Event) -> None:
+        pass
+
+    @command.desc("Get VPS Alive Info")
+    @command.alias("vpsalive")
+    async def cmd_vpsalive(self, ctx: command.Context) -> None:
+        await ctx.respond("HANA-CI VPS is alive")
+
+    @command.desc("Get VPS Info")
+    @command.alias("vinfo")
+    async def cmd_vpsinfo(self, ctx: command.Context) -> None:
+        cpuName = "Docker Container Common CPU"
+        cpuCoreCount = psutil.cpu_count(logical=True)
+        cpuUsage = psutil.cpu_percent(interval=1)
+        diskTotal = int(psutil.disk_usage('/').total/(1024*1024*1024))
+        diskUsed = int(psutil.disk_usage('/').used/(1024*1024*1024))
+        diskAvail = int(psutil.disk_usage('/').free/(1024*1024*1024))
+        diskPercent = psutil.disk_usage('/').percent
+        ramTotal = int(psutil.virtual_memory().total/(1024*1024))
+        ramUsage = int(psutil.virtual_memory().used/(1024*1024))
+        ramFree = int(psutil.virtual_memory().free/(1024*1024))
+        ramUsagePercent = psutil.virtual_memory().percent
+        upTime = subprocess.check_output(['uptime','-p']).decode('UTF-8')
+        msg = '''
+            HANA-CI VPS Info
+--------------------------------
+
+CPU Info
+CPU Name                          = {}
+CPU Core Count               = {} Cores
+CPU Usage                          = {} %
+CPU Uptime                       = {}
+RAM Info
+RAM Total Capacity         = {} MB
+RAM Total Usage              = {} MB | {} %
+RAM Total Free                 = {} MB
+
+Storage Info
+Storage Total Capacity    = {} GB
+Storage Total Usage         = {} GB | {} %
+Storage Total Free            = {} GB\n'''.format(cpuName,cpuCoreCount,cpuUsage,upTime,ramTotal,ramUsage,ramUsagePercent,ramFree,diskTotal,diskUsed,diskPercent,diskAvail)
+        await ctx.respond(msg)
+
+    @command.desc("Send HANA CI VPS Log")
+    @command.alias("vpslog")
+    async def cmd_vpslog(self, ctx: command.Context) -> None:
+        await ctx.respond("Sending logs...")
+        try:
+             print(subprocess.run(["/app/pyrobud/sendLaravelLog.sh", ""],capture_output=True))
+        except subprocess.CalledProcessError as e:
+             if e.output.startswith('error: {'):
+                error = json.loads(e.output[7:])
+                await ctx.respond("Error: "+error['message'])
+             else:
+                await ctx.respond("Error: "+e.output)
